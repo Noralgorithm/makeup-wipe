b65892bf54257083021309e93bd21081a60a7479
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-07-11 20:53:48 -0400

    feat: common words dictionary and letters frequency

---
+++ features/monoalphabetic-cipher/frequency-attack.ts
@@ -0,0 +1,34 @@
+function startFrequencyAttack(message: string): string {
+  let decryptedMessage = "";
+
+  return decryptedMessage;
+}
+
+const LETTERS_FREQUENCY = {
+  E: 120,
+  T: 90,
+  A: 80,
+  I: 80,
+  N: 80,
+  O: 80,
+  S: 80,
+  H: 60,
+  R: 60,
+  D: 40,
+  L: 40,
+  U: 30,
+  C: 30,
+  M: 30,
+  F: 20,
+  W: 20,
+  Y: 20,
+  G: 17,
+  P: 17,
+  B: 16,
+  V: 12,
+  K: 8,
+  Q: 5,
+  J: 4,
+  X: 4,
+  Z: 2,
+};
